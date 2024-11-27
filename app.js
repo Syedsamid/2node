2147a2debb9c9fc7ebea3a4e248bebc541779d0f
Syedsamid <samidsyed1720@gmail.com>
2024-11-27 01:48:05 +0000

    chalk packages

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// const test = require("chalk"); //* es5
+
+import chalk from 'chalk'; //* es6
+console.log(chalk.bgBlack("Hello"));
+
+console.log(chalk.green("***********************************************************************"));
+console.log(chalk.green("***********************************************************************"));
+console.log(chalk.green("**                                                                   **"));
+console.log(chalk.green("**                                                                   **"));
+console.log(chalk.yellow.underline("                      Hydreadabadi Briyanii                 "));
+//console.log(chalk.underline.rgb("---------------------------------------------------------------"));
+
+console.log(chalk.red.bgBlue("                                     "));
+console.log(chalk.bgWhite("              🏳️‍⚧️                     "));
+console.log(chalk.bgBlue("                                     "));
